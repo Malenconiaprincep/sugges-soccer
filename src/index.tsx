489f181194deac9922392e97b60866e3ee362d97
bo.wang <bo.wang@eeoa.com>
2022-08-05 22:19:25 +0800

    chore: update

--- src/index.tsx
+++ src/index.tsx
@@ -6,11 +6,14 @@ import App from "./App"
 import reportWebVitals from "./reportWebVitals"
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//     {/* <GenerateApp /> */}
+//   </React.StrictMode>
+// )
 root.render(
-  <React.StrictMode>
-    <App />
-    {/* <GenerateApp /> */}
-  </React.StrictMode>
+  <App />
 )
 
 // If you want to start measuring performance in your app, pass a function
